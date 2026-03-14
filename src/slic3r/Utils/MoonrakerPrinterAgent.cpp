414ed380a04e3e68dbc103d8e39e4e883404d037
robotfishe <64608682+robotfishe@users.noreply.github.com>
2026-03-14 15:04:30 +0000

    Fixed variable name for HH

--- src/slic3r/Utils/MoonrakerPrinterAgent.cpp
+++ src/slic3r/Utils/MoonrakerPrinterAgent.cpp
@@ -997,7 +997,7 @@ bool MoonrakerPrinterAgent::fetch_hh_filament_info(std::vector<AmsTrayData>& tra
 
             if (tray.tray_type.empty()) {
                 tray.tray_info_idx = "";
-                BOOST_LOG_TRIVIAL(debug) << "Filament sync: Tray " << lane_index << " is empty. Skipping search.";
+                BOOST_LOG_TRIVIAL(debug) << "Filament sync: Tray " << gate_idx << " is empty. Skipping search.";
             }
 
             if (prefer_custom == true) {
